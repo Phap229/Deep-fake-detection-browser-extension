3f8bf3d1b56e078f82252570b2cd5c0a9eb2fc33
Phap229 <133080131+Phap229@users.noreply.github.com>
2025-08-25 15:56:51 +0700

    Delete content.js

--- content.js
+++
@@ -1,5 +0,0 @@
-// Content script for Face Selection Deepfake Detector Extension
-console.log('Face Selection Deepfake Detector content script loaded');
-
-// This content script is loaded on all pages but the actual selection overlay
-// is injected dynamically when needed by the popup script
